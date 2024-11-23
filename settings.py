79a9c553629392f46eb6c65d9e6448c065966178
= <nicholas.waslander@gmail.com>
2024-11-23 14:58:53 -0500

    Effective settings

--- settings.py
+++ settings.py
@@ -9,11 +9,11 @@ settings = {
 'roundsperprint': 580,
 'updatetime': 100,
 'testpercent': 5,
-'batchsize': 200,
+'batchsize': 500,
 
 'weightlearningrate': 1,
 'biaslearningrate': 0.001,
-'descentfactor':0.99994  #The closer to 1, the slower the descent (if greater than one learnrate increases)
+'descentfactor':0.99996  #The closer to 1, the slower the descent (if greater than one learnrate increases)
 }
 
 settings['neuroncounts'].append(settings['outputcount'])
@@ -23,18 +23,18 @@ settings['neuroncounts'] = [settings['inputcount']]+settings['neuroncounts']
 settings2 = {
 'inputcount': 2,
 'outputcount': 1,
-'neuroncounts': [60]*5,
+'neuroncounts': [90]*5,
 'acfunction': 'tanh',
 'lowerval': -1,
 
 'roundsperprint': 580,
 'updatetime': 100,
 'testpercent': 5,
-'batchsize': 400,
+'batchsize': 500,
 
 'weightlearningrate': 1,
 'biaslearningrate': 0.001,
-'descentfactor':0.99994  #The closer to 1, the slower the descent (if greater than one learnrate increases)
+'descentfactor':0.99996  #The closer to 1, the slower the descent (if greater than one learnrate increases)
 }
 
 settings2['neuroncounts'].append(settings2['outputcount'])
